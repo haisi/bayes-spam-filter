9e7f0ef4e32b4a5c8c8214df4a3649dd067e81d3
Hasan Kara <hasan.kara@students.fhnw.ch>
2017-10-16 21:49:33 +0200

    Add posibility to load external mail for assisted learning
    This way the user can load an external mail and decide if the classifier
    was correct with is classification.

--- src/main/java/ch/fhnw/dist/Main.java
+++ src/main/java/ch/fhnw/dist/Main.java
@@ -1,6 +1,11 @@
 package ch.fhnw.dist;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author Hasan Kara <hasan.kara@students.fhnw.ch>
@@ -15,13 +20,28 @@ public class Main {
 
     private final ClassifierTester classifierTester = new ClassifierTester(fileHelper, classifier);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Main().start();
     }
 
-    private void start() {
+    private void start() throws IOException {
 
         classifier.learn();
+
+        Path path = Paths.get("./test-mail");
+        String testMailContent = new String(Files.readAllBytes(path));
+        System.out.println(testMailContent);
+        System.out.println("Spam probability: " + classifier.totalSpamProbability(testMailContent));
+
+        System.out.print("Do you agree? (y/n)");
+        Scanner sc = new Scanner(System.in);
+        String answer = sc.next();
+        if ("y".equals(answer)) {
+            
+        } else {
+
+        }
+
 
         double bestAlpha = scorer.calculateBestAlpha();
         System.out.println("Best Alpha: " + bestAlpha);
