f2ec268c76faa1b788852fda8a56bb832befd955
Hasan Kara <hasan.kara@students.fhnw.ch>
2017-10-16 18:46:17 +0200

    Refactor out FileHelper

--- src/main/java/ch/fhnw/dist/Main.java
+++ src/main/java/ch/fhnw/dist/Main.java
@@ -13,6 +13,8 @@ public class Main {
     final Map<String, HamSpamTuple> words = new HashMap<>();
     final Tokenizer tokenizer = new WhitespaceTokenizer();
 
+    final FileHelper fileHelper = new FileHelper();
+
     private double spamProbability = 0.5d;
     private double hamProbability = 1 - spamProbability;
 
@@ -41,10 +43,10 @@ public class Main {
         });
 
         System.out.println("\n---------------TOTAL SPAM PROBABILITY FOR SPAM MAIL:---------------");
-        System.out.println(totalSpamProbability(getFileContent("data/spam-kallibrierung/00040.949a3d300eadb91d8745f1c1dab51133"), finalNumberOfSpamMails, finalNumberOfHamMails));
+        System.out.println(totalSpamProbability(fileHelper.getFileContent("data/spam-kallibrierung/00040.949a3d300eadb91d8745f1c1dab51133"), finalNumberOfSpamMails, finalNumberOfHamMails));
 
         System.out.println("\n---------------TOTAL SPAM PROBABILITY FOR HAM MAIL:---------------");
-        System.out.println(totalSpamProbability(getFileContent("data/ham-kallibrierung/0001.ea7e79d3153e7469e7a9c3e0af6a357e"), finalNumberOfSpamMails, finalNumberOfHamMails));
+        System.out.println(totalSpamProbability(fileHelper.getFileContent("data/ham-kallibrierung/0001.ea7e79d3153e7469e7a9c3e0af6a357e"), finalNumberOfSpamMails, finalNumberOfHamMails));
     }
 
     /**
@@ -101,10 +103,10 @@ public class Main {
     }
 
     private int countWords(String folder, boolean spam) {
-        List<String> mailList = getFiles(folder);
+        List<String> mailList = FileHelper.getFiles(folder);
         mailList.stream()
                            .map(fileName -> folder + "/" + fileName)
-                           .map(this::getFileContent) // Read Mail content
+                           .map(fileHelper::getFileContent) // Read Mail content
                            .map(tokenizer::getTokens) // tokenize (returns String[])
                            .map(array -> new HashSet<>(Arrays.asList(array)).toArray(new String[0])) // Remove duplicates tokens in mail
                            .flatMap(Arrays::stream)
@@ -130,46 +132,5 @@ public class Main {
             value.incrementHam();
         }
     }
-
-    private List<String> getFiles(String folder) {
-        List<String> fileNames = new ArrayList<>();
-        for (File file : getResourceFolderFiles(folder)) {
-            fileNames.add(file.getName());
-        }
-        return fileNames;
-    }
-
-    private static File[] getResourceFolderFiles(String folder) {
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        URL url = loader.getResource(folder);
-        String path = url.getPath();
-        return new File(path).listFiles();
-    }
-
-    private String getFileContent(String fileName) {
-
-        StringBuilder result = new StringBuilder("");
-
-        //Get file from resources folder
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource(fileName).getFile());
-
-        try (Scanner scanner = new Scanner(file)) {
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                result.append(line).append("\n");
-            }
-
-            scanner.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return result.toString();
-
-    }
-
 
 }
